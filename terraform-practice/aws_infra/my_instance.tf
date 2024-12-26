6fcc2e9203fc6926f1f7d8ce5e53014da844e99c
adity02 <iamadity05@gmail.com>
2024-12-26 20:41:06 +0530

    changes in the main.tf changed the my variables and my instance also

--- terraform-practice/aws_infra/my_instance.tf
+++ terraform-practice/aws_infra/my_instance.tf
@@ -3,7 +3,7 @@
 resource "aws_instance" "my-instance" {
     count = var.instance_count
     ami = var.ami_id
-    instance_type = var.ami_id
+    instance_type = var.instance_type
     tags = {
         Name = "${var.my-env}-tws-free-devops-bootcamp-instance"
     }    
